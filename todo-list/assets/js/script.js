6e753a9807f9571430cf642774f969ea896ca437
Michelle Huang <mhuangsmailbox@gmail.com>
2016-05-02 16:59:52 -0700

    [fix] i class in spans

--- todo-list/assets/js/script.js
+++ todo-list/assets/js/script.js
@@ -1,9 +1,9 @@
-$("ul").on("click", "li", function() {
+$("ul").on("click", "#last", function() {
 	$(this).find("input:text").toggleClass("completed");
 	$(this).find("i:last").toggleClass("fa-square-o fa-check-square-o");
 });
 
-$("ul").on("click", "span", function(event) {
+$("ul").on("click", "#first", function(event) {
 	$(this).parent().fadeOut('500',function(){
 		$(this).remove();
 	})
@@ -14,7 +14,7 @@ $("input").on("keypress", function(event) {
 	if(event.which === 13) {
 		var newTodo = $(this).val();
 		$(this).val("");
-		$("ul").append("<li><span><i class='fa fa-times' aria-hidden='true'></i></span><i class='fa fa-square-o'></i><input class='todo' type='text' value='" +newTodo+ "'></li>");
+		$("ul").append("<li><span id='first'><i class='fa fa-times' aria-hidden='true'></i></span><span id='last'><i class='fa fa-square-o'></i></span><input class='todo' type='text' value='" +newTodo+ "'></li>");
 	}
 });
 
