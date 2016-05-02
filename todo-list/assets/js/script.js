c85b29cf921ed33b6f021aeca21115a8355468f0
Michelle Huang <mhuangsmailbox@gmail.com>
2016-05-01 23:33:28 -0700

    [fix] checkbox styling with font-awesome icons

--- todo-list/assets/js/script.js
+++ todo-list/assets/js/script.js
@@ -5,23 +5,18 @@ $("ul").on("click", "span", function(event) {
 	event.stopPropagation();
 });
 
-$("ul").on("click", "input:checkbox", function() {
-	if($(this).checked) {
-		$(this).next().toggleClass("completed");
-	}
-	else { 
-		$(this).next().toggleClass("completed");
-	}
+$("ul").on("click", "i", function() {
+	$(this).next().toggleClass("completed");
+	$(this).toggleClass("fa-square-o fa-check-square-o");
 });
 
 $("input").on("keypress", function(event) {
 	if(event.which === 13) {
 		var newTodo = $(this).val();
 		$(this).val("");
-		$("ul").append("<li><input type='checkbox'><input class='todo' type='text' value='" +newTodo+ "'><span><i class='fa fa-times' aria-hidden='true'></i></span></li>");
+		$("ul").append("<li><i class='fa fa-square-o'></i><input class='todo' type='text' value='" +newTodo+ "'><span><i class='fa fa-times' aria-hidden='true'></i></span></li>");
 	}
 });
-
 
 
 
