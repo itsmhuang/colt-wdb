5469e72f156db99353ab9339a1fb518a2c9d0a47
Michelle Huang <mhuangsmailbox@gmail.com>
2016-05-02 18:08:05 -0700

    [add] delete via backspace functionality

--- todo-list/assets/js/script.js
+++ todo-list/assets/js/script.js
@@ -1,16 +1,30 @@
+//check checkbox to complete todo
 $("ul").on("click", "#last", function() {
 	$(this).parent().find("input:text").toggleClass("completed");
 	$(this).find("i:last").toggleClass("fa-square-o fa-check-square-o");
 });
 
+//delete todos via x icon
 $("ul").on("click", "#first", function(event) {
 	$(this).parent().fadeOut('500',function(){
 		$(this).remove();
-	})
+	});
 	event.stopPropagation();
 });
 
-$("input").on("keypress", function(event) {
+//delete empty todos via backspace
+$("ul").on("keyup", ".todo", function(event) {
+	//if backspace on empty text input
+	if(event.which === 8 && $(this).val() === "") {
+		$(this).parent().fadeOut('10',function(){
+			$(this).remove();
+		});	
+	}	
+		event.stopPropagation();
+});
+
+//add new todo
+$("#new-todo").on("keypress", function(event) {
 	if(event.which === 13) {
 		var newTodo = $(this).val();
 		$(this).val("");
@@ -18,6 +32,10 @@ $("input").on("keypress", function(event) {
 	}
 });
 
-
-
-
+//add new todo via ul
+// $("ul").on("keypress", ".todo", function(event) {
+// 	if(event.which === 13) {
+// 		var newTodo = "";
+// 		$(this).parent().append("<li><span id='first'><i class='fa fa-times' aria-hidden='true'></i></span><span id='last'><i class='fa fa-square-o'></i></span><input class='todo' type='text' value='" +newTodo+ "'></li>");
+// 	}
+// });
